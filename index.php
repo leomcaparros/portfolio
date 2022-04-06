f2bcbde357fe8927501e39861bab6ce0e3eb0a58
leomcaparros <leomcaparros@gmail.com>
2022-04-05 21:48:53 -0300

    cambios

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <link rel="stylesheet" href="css/Fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="css/Fontawesome/css/fontawesome.min.css">
+    <link rel="stylesheet" href="css/Bootstrap/css/bootstrap.min.css">
+    <i class="fa-solid fa-bars"></i>
+    <script src="css/Bootstrap/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+<body id="Inicio">
+    <header class="container">
+        <nav class="navbar navbar-expand-md  mb-4">
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
+                    aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="fa-solid fa-bars"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                        <li class="nav-item">
+                            <a class="nav-link  py-1 px-3 px-sm-4 active" aria-current="page" href="index.php">Inicio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link  py-1 px-3 px-sm-4" href="sobre-mi.php">Sobre mí</a>
+                        </li>
+                        <li class="nav-item ">
+                            <a class="nav-link  py-1 px-3 px-sm-4" href="proyectos.php">Proyectos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link py-1 px-3 px-sm-4" href="contacto.php">Contacto</a>
+                        </li>
+                    </ul>
+                    <div class="d-flex">
+                        <a class="btn btn-rojo"><i class="fa-solid fa-file-arrow-down"></i> Descargar mi CV</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
+    
+</body>
+<footer class="container">
+        <div class="row mt-5 pb-3">
+            <div class="col-12 col-sm-3 text-center text-sm-start">
+                <a href="https://github.com" target="_blank" title="Github"><i class="fab fa-github"></i></a>
+                <a href="https://www.linkedin.com/in/leomcaparros/" target="_blank" title="Linkedin"><i
+                        class="fab fa-linkedin-in"></i></a>
+            </div>
+            <div class="col-12 col-sm-3 text-center text-sm-start pt-2 p-sm-0">
+                Sponsor <a href="https://depcsuite.com" target="_blank" title="DEPCSUITE">DePC Suite</a>
+            </div>
+            <div class="col-12 col-sm-3 text-sm-start  pt-2 p-sm-0">
+                <a href="mailto:leomcaparros@gmail.com" target="_blank">leomcaparros@gmail.com</a>
+            </div>
+        </div>
+    </footer>
+    <div class="whatsapp">
+        <a href="https://api.whatsapp.com/send?phone=541157236816" title="Whatsapp" target="_blank"><i class="fab fa-whatsapp"></i></a>
+    </div>
+    <script src="css/bootstrap/js/bootstrap.bundle.min.js"></script></body>
+</html>
