a96d4191480d100521d6ab8e92dd41fcdfc11cc2
leomcaparros <leomcaparros@gmail.com>
2022-04-19 20:49:46 -0300

    cambios

--- proyectos.php
+++ proyectos.php
@@ -77,22 +77,5 @@ $pg = "proyectos"; ?>
             </div>
         </div>
     </main>
-    <footer class="container">
-        <div class="row mt-5 pb-3">
-            <div class="col-12 col-sm-3 text-center text-sm-start">
-                <a href="https://github.com" target="_blank" title="Github"><i class="fab fa-github"></i></a>
-                <a href="https://www.linkedin.com/in/leomcaparros/" target="_blank" title="Linkedin"><i
-                        class="fab fa-linkedin-in"></i></a>
-            </div>
-            <div class="col-12 col-sm-3 text-center text-sm-start pt-2 p-sm-0">
-                Sponsor <a href="https://depcsuite.com" target="_blank" title="DEPCSUITE">DePC Suite</a>
-            </div>
-            <div class="col-12 col-sm-3 text-sm-start  pt-2 p-sm-0">
-                <a href="mailto:leomcaparros@gmail.com" target="_blank">leomcaparros@gmail.com</a>
-            </div>
-        </div>
-    </footer>
-    <div class="whatsapp">
-        <a href="https://api.whatsapp.com/send?phone=541157236816" title="Whatsapp" target="_blank"><i class="fab fa-whatsapp"></i></a>
-    </div>
+    <?php include_once "footer.php"; ?>
 </html>
