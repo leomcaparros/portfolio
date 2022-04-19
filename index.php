43a2a231db2d5ccaf1ed2c9428ca8b58d9486f4b
leomcaparros <leomcaparros@gmail.com>
2022-04-19 20:42:34 -0300

    cambios

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php
+$pg = "index"; ?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -13,36 +15,8 @@
     <link rel="stylesheet" href="css/estilos.css">
 </head>
 <body id="inicio">
-    <header class="container">
-        <nav class="navbar navbar-expand-md  mb-4">
-            <div class="container-fluid">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
-                    aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                    <i class="fa-solid fa-bars"></i>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarCollapse">
-                    <ul class="navbar-nav me-auto mb-2 mb-md-0">
-                        <li class="nav-item">
-                            <a class="nav-link  py-1 px-3 px-sm-4 active" aria-current="page" href="index.php">Inicio</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link  py-1 px-3 px-sm-4" href="sobre-mi.php">Sobre mí</a>
-                        </li>
-                        <li class="nav-item ">
-                            <a class="nav-link  py-1 px-3 px-sm-4" href="proyectos.php">Proyectos</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link py-1 px-3 px-sm-4" href="contacto.php">Contacto</a>
-                        </li>
-                    </ul>
-                    <div class="d-flex">
-                        <a class="btn btn-rojo" href="Cv Profesional Leo Caparros.pdf"><i class="fa-solid fa-file-arrow-down"></i> Descargar mi CV</a>
-                    </div>
-                </div>
-            </div>
-        </nav>
-    </header>
-    <main class="container">
+<?php include_once "header.php"; ?>
+<main class="container">
         <div class="row">
             <div class="col-12 mt-4 text-center div-cohete">
                 <a href="proyectos.php"><img src="images/cohete.svg" class="cohete"></a>
--- sobre-mi.php
+++ sobre-mi.php
@@ -1,3 +1,5 @@
+<?php
+$pg = "sobre-mi"; ?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -13,35 +15,7 @@
     <link rel="stylesheet" href="css/estilos.css">
 </head>
 <body id="sobre-mi">
-    <header class="container">
-        <nav class="navbar navbar-expand-md  mb-4">
-            <div class="container-fluid">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
-                    aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                    <i class="fa-solid fa-bars"></i>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarCollapse">
-                    <ul class="navbar-nav me-auto mb-2 mb-md-0">
-                        <li class="nav-item">
-                            <a class="nav-link  py-1 px-3 px-sm-4" aria-current="page" href="index.php">Inicio</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link  py-1 px-3 px-sm-4 active" href="sobre-mi.php">Sobre mi</a>
-                        </li>
-                        <li class="nav-item ">
-                            <a class="nav-link  py-1 px-3 px-sm-4" href="proyectos.php">Proyectos</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link py-1 px-3 px-sm-4" href="contacto.php">Contacto</a>
-                        </li>
-                    </ul>
-                    <div class="d-flex">
-                        <a class="btn btn-rojo" href="Cv Profesional Leo Caparros.pdf"><i class="fa-solid fa-file-arrow-down"></i> Descargar mi CV</a>
-                    </div>
-                </div>
-            </div>
-        </nav>
-    </header>
+<?php include_once "header.php"; ?>
     <main>
         <div class="container">
             <div class="row">
