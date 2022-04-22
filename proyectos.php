8f0506cd154babe77f0abb4b561b2ad47944f355
leomcaparros <leomcaparros@gmail.com>
2022-04-21 21:19:45 -0300

    cambios

--- proyectos.php
+++ proyectos.php
@@ -6,7 +6,7 @@ $pg = "proyectos"; ?>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contacto</title>
+    <title>Proyectos</title>
     <link rel="stylesheet" href="css/Fontawesome/css/all.min.css">
     <link rel="stylesheet" href="css/Fontawesome/css/fontawesome.min.css">
     <link rel="stylesheet" href="css/Bootstrap/css/bootstrap.min.css">
